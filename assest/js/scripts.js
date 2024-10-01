24bacf8a3cbad63362de6e672c80b5757a97e40d
Sumit Girwal <sumitgirwals@gmail.com>
2024-10-01 21:57:05 +0530

    Completed the welcome and home page design

---
+++ assest/js/scripts.js
@@ -0,0 +1,10 @@
+// Optional: Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
